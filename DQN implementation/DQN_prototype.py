f389847f06b74a4f1bbd99b1e09ff368fae7c02e
caspien6 <komjatiede@gmail.com>
2018-02-12 00:41:00 +0100

    Pszeudo újragondolás
    Pythonizáltabb kód, már csak a gradient descent step a kérdéses.

--- DQN implementation/DQN_prototype.py
+++ DQN implementation/DQN_prototype.py
@@ -1,23 +1,47 @@
-D = [x for x in range(1,10) ] #Experience replay dataset (st,at,rt,st+1)
+from skimage.transform import resize
+from random import randint
+
+
+def preprocess_image(image):
+    return resize(image, (84,84))
+
+def argmax(Q, observation):
+	#tfh. az all_action-ben az összes action kombináció benne van, pl. egy eleme: [0,0,0,0,0,0]
+	max_rew = 0
+	for action in all_action:
+		rew = Q(picture, action)
+		if rew > max_rew:
+			max_action = action
+			max_rew = rew
+	return max_action
+
+#a pszeudo kód kicsit átdolgozása innen kezdődik--------------------------------------
+
+
+D = collections.deque(N*[0], maxlen = 5) #Experience replay dataset (st,at,rt,st+1)
 Q = keras.random(10,10)#ez lesz a kezdo Neuralis Halo random ertekekkel feltoltese
+Q2 = Q.deepcopy()
 for epoch in range(1,10):
     env = gym.make('mario')
     action = env.action_space.sample() # your agent here (this takes random actions)
     observation, reward, done, info = env.step(action)
-    #itt meg hianyzik a kepelofeldolgozas f[0] = f(observation)
-    for time in range(1,1000):
-		#ha E < 0.4 akkor random action egyebkent
-		#a[time] = maximumkeresés Q(f(s[time]), a[0...N])
-		x[time+1], reward, done, info = env.step(a[time])
-		
-		# az alabbi reszt nem teljesen ertem, ez azt akarja jelenteni, hogy
-		#a kovetkezo allapot az elozo allapot + action es hozzaadodik a
-		#mostani kep?
-		s[time+1] = s[time],a[time], x[time+1] 
-		
-		D.push((f[time], a[time], r[time], f[time+1]) #az ujrafelhasznalas celjabol felvesszuk a D-be
-		j = int.random(1,100)
-		rj = D[j][2] #a 2es index a reward lesz
+    preprocessed_img = preprocess_image(observation) #Kepelofeldolgozas
+    for t in range(1,T):#T itt egy jól megválasztott maximum szám, vagy lehet hogy inkább a done-ra lenne szükség?
+		if E < randint(0, 100):
+			action_t = env.action_spacle.sample()
+		else:
+			action_t = argmax(Q,preprocessed_img)
+		observation, reward, done, info = env.step(action_t)
+		preprocessed_img2 = preprocess_image(observation)
+		D.push((preprocessed_img, action_t, reward, preprocessed_img2))
+		minibatch = random.sample(D, 1) #minibatch = (s t, a t, r t, s t+1)
+		if minibatch[2]:
+			yj = minibatch[1] #ha a done true volt akkor csak a rewardra figyelünk
+		else:
+			yj = minibatch[1] + learning_factor*argmax(Q2,minibatch[3]) #a tanulási tényező egy konstans csak 0 és 1 között.
+		#gradient decent step
+		if t %100:
+			Q2 = Q.deepcopy()
 		
 
 		
